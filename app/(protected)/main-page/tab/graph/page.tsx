f23a88747d695da9cd41e05ebdc6098978a5fff3
ranmaru <ranamimamimi@gmail.com>
2025-12-22 20:42:14 +0900

    tab6グラフ修正

--- app/(protected)/main-page/tab/graph/page.tsx
+++ app/(protected)/main-page/tab/graph/page.tsx
@@ -1,4 +1,4 @@
-import Tab1 from '@/components/tab/tab1/tab-1';
+import Tab6 from '@/components/tab/tab1/tab-1';
 import { prisma } from '@/lib/prisma';
 import { ArrayItemSchema } from '@/schemas/api/tab-1';
 import { itemsFromBigintToString } from '@/utils/itemsFromBigintToString';
@@ -15,5 +15,5 @@ export default async function Page() {
   const serialized = itemsFromBigintToString(items);
   const parsedData = ArrayItemSchema.parse(itemsFromBigintToString(serialized));
 
-  return <Tab1 tab1Data={parsedData} />;
+  return <Tab6 tab1Data={parsedData} />;
 }
