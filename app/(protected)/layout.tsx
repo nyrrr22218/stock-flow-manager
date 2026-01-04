91e4f5efa1c0accb39cd8ba254bdb271e1f3ee3d
ranmaru <ranamimamimi@gmail.com>
2026-01-04 12:37:22 +0900

    ログアウト後,戻るボタンを押してもログイン後の画面が出ないように修正

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -2,15 +2,19 @@ import Header from '@/components/header';
 import Footer from '@/components/footer';
 import { Box } from '@mui/material';
 import { createClient } from '@/lib/supabase-server';
+import { redirect } from 'next/navigation';
 
 export default async function ProtectedLayout({ children }: { children: React.ReactNode }) {
   const supabase = await createClient();
   const {
     data: { user },
   } = await supabase.auth.getUser();
+  if (!user) {
+    redirect('/sign-in');
+  }
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-      <Header userEmail={user?.email ?? ''} />
+      <Header userEmail={user.email ?? ''} />
       <Box component="main" sx={{ flexGrow: 1, minHeight: 'calc(100vh - 120px)' }}>
         {children}
       </Box>
